3e0714bf08e8f6e4dd7a5f1bbbd03404b0024f25
Vulcanior <theo@minacori.fr>
2017-03-28 13:36:24 +0200

    Début

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 from naoqi import ALProxy
-# from enum import Enum
+from enum import Enum
+import copy
+# http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+# pip install enum34
 
-# class Case(Enum):
-	# VIDE = 0
-	# PION_BLANC = 1
-	# PION_NOIR = 2
-	# DAME_BLANCHE = 3
-	# DAME_NOIRE = 4
+class Case(Enum):
+	VIDE = 0
+	PION_BLANC = 1
+	PION_NOIR = 2
+	DAME_BLANCHE = 3
+	DAME_NOIRE = 4
 	
-# Plateau = [[0 for x in range(10)] for y in range(10)] 
+class Plateau:
+	def __init__(self):
+		
+	
+Plateau = [[0 for x in range(10)] for y in range(10)] 
+
+del evalution(plateau):
+	return 0
 
 tts = ALProxy("ALTextToSpeech", "127.0.0.1", 50766)
 tts.say("Hello, world!")
