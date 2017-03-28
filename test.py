f11bc6097a1f563237c8bc03fde3c86c4cb461ee
Vulcanior <theo@minacori.fr>
2017-03-28 16:04:16 +0200

    hklkjhgf

--- test.py
+++ test.py
@@ -24,6 +24,11 @@ class Tour(Enum):
 	BLANC = 0
 	NOIR = 1
 	AUCUN = 2
+
+class Action(Enum):
+	AUCUN = 0
+	MANGER = 1
+	BOUGER = 2
 	
 def owner(case):
 	if Case.PION_BLANC == case or Case.DAME_BLANCHE == case:
@@ -33,9 +38,17 @@ def owner(case):
 	else
 		return Tour.AUCUN
 		
+def notOwner(case, pdv):
+	tmp = owner(case)
+	if(tmp != pdv and tmp != Tour.AUCUN):
+		return true
+	else
+		return false
+		
 class Coup:
 	coup_suivant = None
 	pdv = Tour.AUCUN
+	action = 
 	
 class Plateau:
 	coups = 0
@@ -44,7 +57,7 @@ class Plateau:
 	nbr_pions_blancs = 20
 	nbr_pions_noirs = 20
 	
-	obj = [[0 for x in range(10)] for y in range(10)] 
+	obj = [[0 for x in range(10)] for y in range(10)] # 0,0 <=> en bas à gauche
 	
 	def __init__(self):
 		
@@ -66,15 +79,32 @@ class Plateau:
 		coups = []
 		for i in range(0, 10):
 			for j in range(0, 10):
-				if(owner(obj[i][j]) == pdv):
+				if(owner(self.obj[i][j]) == pdv):
 					coups += makeMoves(i, j, pdv)
+		return coups
+		
+	# Un pion ne peut faire qu'avancer s'il ne manger pas
+	def validerBouger(y, pdv):
+		if((pdv == Tour.BLANC and y == 1) or (pdv == Tour.NOIR and y == -1)):
+			return true
+		else
+			return false
+	
+	def canEatOrMove(self, i, j, x, y, pdv):
+		if(0 <= i + x <= 10 and 0 <= j + y <= 10):
+			if(notOwner(self.obj[i + x][j + y]) and 0 <= i + 2 * x < 10 and 0 <= j + 2 * y <= 10):
+				if(owner(self.obj[i + 2 * x][j + 2 * y]) == Tour.AUCUN):
+					return Action.MANGER
+			elif(validerBouger(y, pdv) and owner(self.obj[i + x][j + y]) == Tour.AUCUN):
+				return Action.BOUGER
+		return Action.AUCUN
 	
 	def makeMoves(self, i, j, pdv):
 		liste = []
-		if(i - 1 > 0 and ):
-			
-	
-
+		if(canEat(i, j, -1, -1, pdv)):
+			liste += new Coup()....
+		
+		return liste
 
 
 tts = ALProxy("ALTextToSpeech", "127.0.0.1", 50766)
